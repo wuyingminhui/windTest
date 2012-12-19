e0661c7de3f97f24080de433173a49697381a70c
neekey <ni184775761@gmail.com>
2012-12-19 21:18:27 +0800

    添加首页导航~

--- socketServer/routes/index.js
+++ socketServer/routes/index.js
@@ -4,9 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'WindTest' });
+  res.render('index', { title: 'WindTest', pageName: 'index' });
 };
 
 exports.simulate = function(req, res){
-    res.render('simulate', { title: 'WindTest Client Simulation!' });
+    res.render('simulate', { title: 'WindTest Client Simulation!', pageName: 'simulate' });
 };
