b9a495c29494e599c04d284c45d8e539ce6ff25e
Neekey <ni184775761@gmail.com>
2013-01-01 17:09:47 +0800

    添加IEDriver

--- Selenium/start.cmd
+++ Selenium/start.cmd
@@ -1,2 +1,2 @@
 title selenium server
-java -jar selenium-server-standalone-2.24.1.jar -Dwebdriver.chrome.driver=chromedriver.exe
+java -jar selenium-server-standalone-2.24.1.jar -Dwebdriver.chrome.driver=chromedriver.exe -Dwebdriver.ie.driver=IEDriverServer.exe
